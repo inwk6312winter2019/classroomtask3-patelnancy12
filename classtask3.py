2729cb7d82249b1488c4625328e0db26e30f4942
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-04 14:41:29 +0000

    Task initiated

--- classtask3.py
+++ classtask3.py
@@ -1,4 +1,9 @@
-#class task3 card program
+#Program to implement bluff card game...
+
+rank  = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+suite = ["H", "S", "D", "C"]
 
 
+cards = list(zip(rank,suite))
 
+print(cards)
