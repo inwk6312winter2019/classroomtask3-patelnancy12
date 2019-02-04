e6a6b2212c738d3fbaccb85a4ed0869176dd06e3
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-04 15:05:06 +0000

    Task done

--- classtask3.py
+++ classtask3.py
@@ -1,9 +1,18 @@
-#Program to implement bluff card game...
+class card():
 
-rank  = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-suite = ["H", "S", "D", "C"]
+  """this creates a card object"""
+
+  suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades'] 
+  rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+  def __init__(self,rank=0,suite=2):
+    self.rank = rank
+    self.suite =  suite
+
+  def __str__(self):
+    return f"the rank is {card.rank_names[self.rank]} and the suit is {card.suit_names[self.suite]}"
 
 
-cards = list(zip(rank,suite))
+ace_of_spade = card(1,3)
 
-print(cards)
+print(ace_of_spade)
